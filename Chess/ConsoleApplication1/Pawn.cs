8537b4a92919119cad50acb7aed58dfd0efeef75
BoasLee <bolee@tableau.com>
2016-10-22 14:55:28 -0700

    Upload all files (pawn)

---
+++ Chess/ConsoleApplication1/Pawn.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Pawn : Pieces
+    {
+        //Constructor
+        public Pawn(int X, int Y, Color color) : base(X, Y, color)
+        {
+        }
+
+        // Method
+        public override char GetPiecetype()
+        {
+            return 'p';
+        }
+        public override bool Move(Pieces[,] Board, int X, int Y)
+        {
+            if ((this.Y + 1 == Y && this.X == X && Board[X,Y] == null) || (this.NumberOfMoves== 0 && this.Y + 2 == Y && this.X == X && Board[X, this.Y + 1] == null && Board[X, Y] == null))
+            {
+                Board[this.X, this.Y] = null;
+                this.Y = Y;
+                Board[X, Y] = this;
+                this.NumberOfMoves++;
+                return true;
+            }
+            return false;
+        }
+        public override bool Capture(Pieces[,] Board, int X, int Y)
+        {
+            if ((this.X + 1 == X || this.X - 1 == X ) && this.Y + 1 == Y && Board[X, Y] != null)
+            {
+                if (this.PieceColor == Board[X, Y].GetColor())
+                {
+                    return false;
+                }
+                Board[this.X, this.Y] = null;
+                Board[X,Y] = this;
+                this.Y = Y;
+                this.X = X;
+                NumberOfMoves++;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Chess/ConsoleApplication1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        // "Global Variable"
+        static public Pieces[,] Myboard = new Pieces[8, 8]; 
+     
+        static void Main(string[] args)
+        {
+            ClearBoard();
+            Pieces pawn1 = new Pawn(1, 1, Color.White);
+            Pieces pawn2 = new Pawn(0, 2, Color.Black);
+            Myboard[1, 1] = pawn1;
+            Myboard[0, 2] = pawn2;
+            DisplayPawnCapture(pawn1, 0, 2); // how am i passing a sub class?
+            Console.ReadKey();
+        }
+
+        static void ClearBoard()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Myboard[i, j] = null;
+                }
+            }
+        }
+        static void DisplayBoard()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (Myboard[(j), (7-i)] == null)
+                    {
+                        System.Console.Write('a');
+                    }
+                    else
+                    {
+                        System.Console.Write(Myboard[(j),(7-i)].GetPiecetype());
+                    }
+                }
+                System.Console.WriteLine();
+            }
+        }
+        static void DisplayPawnMove(Pieces pawn, int X, int Y)
+        {
+            DisplayBoard();
+            if (pawn.Move(Myboard, X, Y) == true)
+            {
+                System.Console.WriteLine("Moved =)");
+            }
+            else
+            {
+                System.Console.WriteLine("Did not move =(");
+            }
+            DisplayBoard();
+            System.Console.WriteLine();
+        }
+        static void DisplayPawnCapture(Pieces pawn, int X, int Y)
+        {
+            DisplayBoard();
+            if (pawn.Capture(Myboard, X, Y) == true)
+            {
+                System.Console.WriteLine("Killed you =P");
+            }
+            else
+            {
+                System.Console.WriteLine("Silly Rabbit, Tricks are for Kids");
+            }
+            DisplayBoard();
+            System.Console.WriteLine();
+        }        
+    }
+}
